2fecf04f810907bfc28e0fe95f2d83f361130d74
Zakaria Abourida <zakaria.abourida@edu.unito.it>
2024-06-02 17:09:53 +0200

    Starting project

---
+++ CatERing/src/businesslogic/user/User.java
@@ -0,0 +1,13 @@
+package businesslogic.user;
+
+public class User {
+    public boolean isChef() {
+        // TODO: questo è da implementare, versione di prova
+        return true;
+    }
+
+    public String getUserName() {
+        // TODO da implementare, versione di prova
+        return "Pinco pallino";
+    }
+}
